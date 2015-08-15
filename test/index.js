7f89bc3bcde4bc623d8b1331d1dbffb62335d7bd
metherton <martinetherton63@googlemail.com>
2015-08-15 23:59:45 +0200

    aadded test for PUSH method

--- test/index.js
+++ test/index.js
@@ -1,12 +1,21 @@
 var request = require('supertest');
 var app = require('../app');
 
-describe('GET /sendsms', function(){
-    it('respond with json', function(done){
+describe('/sendsms', function() {
+    
+    it('GET should respond with 200', function(done){
         request(app)
             .get('/sendsms')
             .expect('Content-Type', 'text/html; charset=utf-8')
             .expect(200, done);
 
-    })
+    });
+
+    it('POST should respond with 200', function(done){
+        request(app)
+            .post('/sendsms')
+            .expect('Content-Type', 'text/html; charset=utf-8')
+            .expect(200, done);
+
+    });
 });
